3ca9b4986b7aaa19eaf22df6d23be26cb91f9b59
Orang-Wutang <sharon.brs@web.de>
2025-02-10 18:54:36 +0100

    lila frostedglass background :*

--- Dave/ViewModels/MainWindow.axaml.cs
+++ Dave/ViewModels/MainWindow.axaml.cs
@@ -1,11 +1,24 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 
-namespace Dave.ViewModels;
-
-public partial class MainWindow : Window
+namespace Dave.ViewModels
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDragWindow(object? sender, PointerPressedEventArgs e)
+        {
+            // Prüfen, ob die linke Maustaste gedrückt ist
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            {
+                // BeginMoveDrag benötigt e als Argument
+                BeginMoveDrag(e);
+            }
+        }
     }
 }
