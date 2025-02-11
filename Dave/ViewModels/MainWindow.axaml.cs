54f68938b20ca2b1bde5c09616bce805866411e7
carol7na777 <carolinahoffmann83@gmail.com>
2025-02-11 11:40:43 +0100

    Add minimize-, fullscreen- and closebutton + style

--- Dave/ViewModels/MainWindow.axaml.cs
+++ Dave/ViewModels/MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace Dave.ViewModels
 {
@@ -9,6 +10,30 @@ namespace Dave.ViewModels
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Event-Handler für den Minimieren-Button
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        // Event-Handler für den Vollbildmodus-Button
+        private void FullScreenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
         }
 
         private void OnDragWindow(object? sender, PointerPressedEventArgs e)
