a762096fd691a25ecc65b68fb3eff839e66f10bb
PhilikusHD <philip.dammann06@gmail.com>
2025-02-12 12:49:23 +0100

    moved logic to main function

--- Dave/Program.cs
+++ Dave/Program.cs
@@ -4,6 +4,12 @@ using System;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Runtime.InteropServices;
+using Dave.Modules.Abstract;
+using Dave.Modules.Model;
+using Dave.Modules.Steam;
+using Dave.Modules;
+using System.Collections.Generic;
+using static Dave.Logger.Logger;
 
 namespace Dave;
 
@@ -39,6 +45,41 @@ class Program
 
         // Keep the console window open.
         OpenConsole();  // Call this to open the console
+        InitLog();
+        // Create a list of game launcher modules. For now, only SteamModule is available.
+        List<IGameLauncherModule> modules = new List<IGameLauncherModule>
+            {
+                new SteamModule()
+                // Future modules like new GOGModule() can be added here.
+            };
+
+        // Instantiate the GameManager with the modules.
+        GameManager gameManager = new GameManager(modules);
+
+        Task.Run(async () =>
+        {
+            try
+            {
+                // Fetch the aggregated list of games from all modules.
+                List<Game> allGames = await gameManager.GetAllGamesAsync();
+
+                // Display the list of games.
+                Console.WriteLine("Owned Games:");
+                var sortedGames = allGames.OrderByDescending(g => g.Playtime).ToList();
+                Console.WriteLine("{0,-10} {1,-30} {2,15}", "ID", "Name", "Playtime (h)");
+                Console.WriteLine(new string('-', 60));
+
+                foreach (var game in sortedGames)
+                {
+                    Console.WriteLine("{0,-10} {1,-30} {2,15:F1}", game.ID, game.Name, game.Playtime);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        });
+
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
